96815a267dc2b36fd9dc26cc595cd858cdde68c0
Ev-Hoang <hduckien@gmail.com>
2025-07-11 11:34:53 +0700

    Update

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+
+    created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%SZ")
+
     class Meta:
         model = Post
         fields = '__all__'
+
+    def validate_title(self, value):
+        if not value:
+            raise serializers.ValidationError("You must have a title.")
+        if len(value) > 200:
+            raise serializers.ValidationError("Title must be shorter than 200 characters.")
+        return value
+
+    def validate_content(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError("content must exceeded 10 characters.")
+        return value
+
+    def validate_author(self, value):
+        if not value:
+            raise serializers.ValidationError("You must have an author.")
+        return value
--- api/views.py
+++ api/views.py
@@ -2,16 +2,56 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Post
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from .serializers import PostSerializer
 from django.shortcuts import get_object_or_404
 
 # /api/posts/
 class PostListAPIView(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'search',
+                openapi.IN_QUERY,
+                description="Search by title",
+                type=openapi.TYPE_STRING
+            ),
+            openapi.Parameter(
+                'limit',
+                openapi.IN_QUERY,
+                description="Ammount of blogs",
+                type=openapi.TYPE_INTEGER
+            ),
+            openapi.Parameter(
+                'offset',
+                openapi.IN_QUERY,
+                description="Offsets",
+                type=openapi.TYPE_INTEGER
+            ),
+        ]
+    )
     def get(self, request):
+        # Search theo title
+        search_query = request.GET.get('search')
         posts = Post.objects.all()
+        if search_query:
+            posts = posts.filter(title__icontains=search_query)
+
+        # Pagination: limit & offset
+        limit = int(request.GET.get('limit', 10))
+        offset = int(request.GET.get('offset', 0))
+        total = posts.count()
+
+        posts = posts[offset:offset + limit]
         serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+
+        return Response({
+            "total": total,
+            "limit": limit,
+            "offset": offset,
+            "results": serializer.data
+        })
 
     @swagger_auto_schema(request_body=PostSerializer)
     def post(self, request):
